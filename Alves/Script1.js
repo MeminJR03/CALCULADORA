acae5eba3369485c83d1895b10c178ed1b1b6a2c
Darcad1015 <duvanalves6@gmail.com>
2024-06-21 08:21:13 -0500

    .subir
    se subieron los tres archivos de la calculadora personal

---
+++ Alves/Script1.js
@@ -0,0 +1,24 @@
+// Duvan ALves Rodriguez 1111197732
+// Obtenemos los elementos del DOM
+const resultado = document.getElementById('resultado');
+const botones = document.querySelectorAll('button');
+const limpiar = document.getElementById('limpiar');
+const igual = document.getElementById('igual');
+
+// Iteramos por todos los botones y les asignamos un evento click
+botones.forEach(function (boton) {
+    boton.addEventListener('click', function () {
+        // Si el botón es C, borramos el contenido del input
+        if (this.id === 'limpiar') {
+            resultado.value = '';
+        }
+        // Si el botón es =, evaluamos la expresión matemática en el input
+        else if (this.id === 'igual') {
+            resultado.value = eval(resultado.value);
+        }
+        // En otro caso, agregamos el valor del botón al input
+        else {
+            resultado.value += this.value;
+        }
+    });
+});
